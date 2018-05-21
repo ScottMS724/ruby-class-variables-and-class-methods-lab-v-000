8bb2532974e5e40dd4d9e2e5189f7baa6be867e6
Scott Silverman <scottms724@gmail.com>
2018-05-21 16:28:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,6 @@ class Song
   def self.genre_count 
     genre_count = {} 
     @@genres.each do |x| 
-      genre_count[x] = 1 unless genre_count.include?(x)
       genre_count[x] += 1 
     end 
   end 
